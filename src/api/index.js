d185f9ea762c9b9c8d430278d2cda81f792c5491
ahmadjavaiddev <ahmadjavaiddev@gmail.com>
2024-07-21 03:18:28 +0500

    Add baseApi to handle apis from one place and in one flow

---
+++ src/api/index.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { LocalStorage } from "../lib/localStorage";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+if (!apiUrl) {
+    console.warn("API URL is not available in environment variables");
+}
+const token = LocalStorage.get("accessToken");
+if (!token) {
+    console.warn("Token is not available in local storage");
+}
+
+const apiClient = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
+    timeout: 120000,
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: token ? `Bearer ${token}` : "",
+    },
+});
+
+apiClient.interceptors.request.use(
+    function (config) {
+        // Retrieve user accessToken from local storage
+        const token = LocalStorage.get("accessToken");
+        // Set authorization header with bearer accessToken
+        config.headers.Authorization = `Bearer ${token}`;
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
+
+const loginUser = ({ email, password }) => {
+    return apiClient.post("/users/login", { email, password });
+};
+const registerUser = (user) => {
+    const { firstName, lastName, userName, email, password } = user;
+    return apiClient.post("/users/login", { firstName, lastName, userName, email, password });
+};
+
+const validateUser = () => {
+    return apiClient.get("/users/user");
+};
+
+const getNotifications = () => {
+    return apiClient.get("/notifications/get");
+};
+
+const getTransactions = () => {
+    return apiClient.get("/transactions");
+};
+
+export { loginUser, registerUser, validateUser, getNotifications, getTransactions };
--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -1,13 +1,22 @@
 import axios from "axios";
+import { LocalStorage } from "./localStorage";
 
 const apiUrl = import.meta.env.VITE_API_URL;
+if (!apiUrl) {
+    console.warn("API URL is not available in environment variables");
+}
+const token = LocalStorage.get("accessToken");
+if (!token) {
+    console.warn("Token is not available in local storage");
+}
 
-const axiosInstance = axios.create({
+const api = axios.create({
     baseURL: apiUrl,
-    withCredentials: true, // This ensures cookies are sent with requests
+    // withCredentials: true, // This ensures cookies are sent with requests
     headers: {
         "Content-Type": "application/json",
+        Authorization: token ? `Bearer ${token}` : "",
     },
 });
 
-export default axiosInstance;
+export default api;
