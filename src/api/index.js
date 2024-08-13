54383940c7b926c431a77982ac96c7cac0232aac
ahmadjavaiddev <ahmadjavaiddev@gmail.com>
2024-08-13 00:21:43 -0600

    Fix APi response in api file

--- src/api/index.js
+++ src/api/index.js
@@ -39,17 +39,34 @@ const loginUser = async ({ email, password }) => {
     LocalStorage.set("refreshToken", response.data.data.refreshToken);
     return response.data.data;
 };
-const registerUser = (user) => {
+
+const registerUser = async (user) => {
     const { firstName, lastName, userName, email, password } = user;
-    return apiClient.post("/users/login", { firstName, lastName, userName, email, password });
+    const response = await apiClient.post("/users/login", {
+        firstName,
+        lastName,
+        userName,
+        email,
+        password,
+    });
+    LocalStorage.set("accessToken", response.data.data.accessToken);
+    LocalStorage.set("refreshToken", response.data.data.refreshToken);
+    return response.data.data;
 };
 
-const validateUser = () => {
-    return apiClient.get("/users/user");
+const refreshTokens = async (token) => {
+    const response = await apiClient.post("/users/refresh-token", { refreshToken: token });
+    return response.data.data;
 };
 
-const getNotifications = () => {
-    return apiClient.get("/notifications/get");
+const validateUser = async () => {
+    const response = await apiClient.get("/users/user");
+    return response.data.data;
+};
+
+const getNotifications = async () => {
+    const response = await apiClient.get("/notifications/get");
+    return response.data.data;
 };
 
 const getTransactions = async () => {
@@ -67,8 +84,14 @@ const approveTransaction = async (transactionId) => {
     return response.data.data;
 };
 
-const requestCard = (pinCode) => {
-    return apiClient.post("/card/create", { pinCode: pinCode });
+const rejectRequest = async (transactionId) => {
+    const response = await apiClient.get(`/transactions/requested/reject/${transactionId}`);
+    return response.data.data;
+};
+
+const requestCard = async (pinCode) => {
+    const response = await apiClient.post("/card/create", { pinCode: pinCode });
+    return response.data.data;
 };
 
 const getCardDetails = async () => {
@@ -84,11 +107,13 @@ const verifyUserLogin = async (token) => {
 export {
     loginUser,
     registerUser,
+    refreshTokens,
     validateUser,
     getNotifications,
     getTransactions,
     getRequestedTransactions,
     approveTransaction,
+    rejectRequest,
     requestCard,
     getCardDetails,
     verifyUserLogin,
